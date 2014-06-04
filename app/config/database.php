3ef37a1199b7dba70d6b0e5d8930f9abc0af1c9c
Lonnie Ezell <lonnieje@gmail.com>
2014-06-03 22:22:25 -0500

    Blanking out our database config file.

--- app/config/database.php
+++ app/config/database.php
@@ -49,10 +49,10 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'bonfire';
+$db['default']['username'] = '';
 $db['default']['password'] = '';
-$db['default']['database'] = 'bonfire';
-$db['default']['dbdriver'] = 'mysql';
+$db['default']['database'] = '';
+$db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = 'bf_';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
@@ -61,8 +61,8 @@ $db['default']['cachedir'] = '';
 $db['default']['char_set'] = 'utf8';
 $db['default']['dbcollat'] = 'utf8_general_ci';
 $db['default']['swap_pre'] = '';
-$db['default']['autoinit'] = TRUE;
-$db['default']['stricton'] = TRUE;
+$db['default']['autoinit'] = FALSE;
+$db['default']['stricton'] = FALSE;
 
 
 /* End of file database.php */
